26423b2fa61e40eea03e2e500f57811c597db498
Vanshika Srivastava <vanshikasri.2003@gmail.com>
2024-12-05 17:43:27 +0530

    Added api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "a90e17a18134822225e6e959ba5e57eb";
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
